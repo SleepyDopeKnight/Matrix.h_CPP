f848dc38f40f8b3fe2c9c58de0b05fbe015580d1
Your Name <you@example.com>
2023-05-16 21:16:07 +0300

    style correct

--- src/s21_matrix_oop.cc
+++ src/s21_matrix_oop.cc
@@ -1,7 +1,6 @@
 #include "s21_matrix_oop.h"
 
-S21Matrix::S21Matrix() : rows_(0), cols_(0), matrix_(0) {
-}
+S21Matrix::S21Matrix() : rows_(0), cols_(0), matrix_(0) {}
 
 S21Matrix::S21Matrix(int rows, int cols) {
   if (rows > 0 && cols > 0) {
@@ -20,7 +19,8 @@ S21Matrix::S21Matrix(const S21Matrix& other) {
   }
 }
 
-S21Matrix::S21Matrix(S21Matrix&& other) : rows_(other.rows_), cols_(other.cols_), matrix_(other.matrix_) {
+S21Matrix::S21Matrix(S21Matrix&& other)
+    : rows_(other.rows_), cols_(other.cols_), matrix_(other.matrix_) {
   other.rows_ = 0;
   other.cols_ = 0;
   other.matrix_ = 0;
